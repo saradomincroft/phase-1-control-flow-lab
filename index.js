67b1a4395d912308bf32fd0a26c3b5b00e380a0b
Sara Croft <86280277+saradomincroft@users.noreply.github.com>
2023-12-26 16:14:48 +1100

    Lab completed

--- index.js
+++ index.js
@@ -1,11 +1,30 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+
+function scuberGreetingForFeet(feet, greeting){
+  if (feet <= 400) {
+    greeting = 'This one is on me!'
+  } else if (feet > 400 && feet <= 2000) {
+      greeting = 'That will be twenty bucks.'
+    } else if (feet > 2000 && feet <= 2500) {
+      greeting = 'I will gladly take your thirty bucks.'
+    } else {
+      greeting = 'No can do.'
+    }
+  return greeting
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+function ternaryCheckCity(city){
+  return city === 'NYC' ? 'Ok, sounds good.' : 'No go.'
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(tip){
+  switch(tip) {
+    case 'generous': 
+        return 'Thank you so much.'
+        break;
+    case 'not as generous':
+        return 'Thank you.'
+        break;
+    default:
+        return 'Bye.'
+  }
 }
